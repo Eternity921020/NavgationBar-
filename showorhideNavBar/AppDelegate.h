526a19af7a7c53762251b3ed55898d0beb997323
肖伟华 <1019459067@qq.com>
2015-07-02 12:31:11 +0800

    Initial Commit

---
+++ showorhideNavBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  showorhideNavBar
+//
+//  Created by Eternity° on 15/7/2.
+//  Copyright (c) 2015年 Eternity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ showorhideNavBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  showorhideNavBar
+//
+//  Created by Eternity° on 15/7/2.
+//  Copyright (c) 2015年 Eternity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
